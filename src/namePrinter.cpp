c2e48a99220718079e425dba03dc4f5d91814df7
Ryan <rnel66@gmail.com>
2019-10-04 15:20:12 +0200

    initial commit

---
+++ src/namePrinter.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "namePrinter.h"
+
+void NamePrinter::printname(string geekname) {
+     cout << "Geekname is: " << geekname << endl; 
+}
---
+++ src/namePrinter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+class NamePrinter
+{
+	// Access specifier 
+public:
+
+	// Member Functions() 
+	void printname(string geekname);
+};
